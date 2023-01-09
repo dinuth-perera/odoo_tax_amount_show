950ceeb372ab6c0d64c8a9a13ec14ed701d25ecd
Dinuth Perera <pm@dinuth.me>
2023-01-09 13:35:51 +0530

    initial commit

---
+++ so_tax_amount_show/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sale Order Line Tax Amount",
+    "version": "16.0.0.0.1",
+    "summary": "Show Sale Order Line Tax Amount",
+    "author": "Dinuth Perera",
+    "maintainer": "Dinuth Perera",
+    "website": "http://dinuth.me",
+    "contributors": ["Dinuth Perera"],
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": [
+        "sale"
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+        "report/ir_actions_report_templates.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
